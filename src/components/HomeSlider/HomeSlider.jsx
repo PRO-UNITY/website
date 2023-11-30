5b3a0fe75e95aeb129a54e451c6c469d31df9ca4
Jobir <jobirashurov7877@gmail.com>
2023-11-30 15:23:40 +0500

    commit

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -1,17 +1,20 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { HomeSliderImage } from "../../constants";
 import "./HomeSlider.css";
 
 export default function HomeSwipper() {
   return (
     <>
-      {/* <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {HomeSliderImage.map((item) => (
           <SwiperSlide key={item.id} className="swiper-slide">
             <img src={item.img} alt="slider-image" />
           </SwiperSlide>
         ))}
-      </Swiper> */}
+      </Swiper>
     </>
   );
 }
