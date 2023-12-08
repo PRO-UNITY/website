7aefe7de950ca2a3241d454bf48928adf4d6dbcd
Jobir <jobirashurov7877@gmail.com>
2023-12-08 12:32:58 +0500

    commited

--- src/pages/Products/Hrms/admin/Hrms-home/Hrms-home.jsx
+++ src/pages/Products/Hrms/admin/Hrms-home/Hrms-home.jsx
@@ -32,106 +32,110 @@ const HrmsHome = () => {
               </button>
             ))}
           </div>
-          <table  className="table border ">
-            <tbody>
-              <tr>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <img
-                      width={40}
-                      height={40}
-                      className="me-3 rounded-circle table-profile-img"
-                      src="https://www.growcropsonline.com/assets/img/agent-2.jpg"
-                      alt=""
-                    />
-                    <div className="user-data ">
-                      <div className="d-flex align-items-center">
-                        <h6 className="m-0">Maribel_Koss </h6>
-                        <img
-                          width={28}
-                          src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Twitter-Verified-900x0.png"
-                          alt=""
-                        />
+          <div className="card p-0 rounded overflow-hidden">
+            <table className="table m-0">
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        width={40}
+                        height={40}
+                        className="me-3 rounded-circle table-profile-img"
+                        src="https://www.growcropsonline.com/assets/img/agent-2.jpg"
+                        alt=""
+                      />
+                      <div className="user-data ">
+                        <div className="d-flex align-items-center">
+                          <h6 className="m-0">Maribel_Koss </h6>
+                          <img
+                            width={28}
+                            src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Twitter-Verified-900x0.png"
+                            alt=""
+                          />
+                        </div>
+                        <span className="text-secondary">@maribelk</span>
                       </div>
-                      <span className="text-secondary">@maribelk</span>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">Oct 01, 2022 </h6>
-                  <span className="text-secondary ">Subscribe Date</span>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">
-                    None
-                    <i className="fa-solid fa-circle-exclamation mx-1 text-warning"></i>
-                    <span className="text-warning fw-normal">Expires soon</span>
-                  </h6>
-                  <span className="text-secondary">Renew Date</span>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">$3.00</h6>
-                  <span className="text-secondary">Monthly Cost</span>
-                </td>
-                <td align="center" className="text-end">
-                  <div className="d-flex align-items-center justify-content-end mt-2">
-                    <button className="action-button btn py-2  fw-semibold border">
-                      <i className="fa-solid fa-rotate px-1"></i> Renew
-                    </button>
-                    <button className="btn ms-1">
-                      <i className="fa-solid fa-ellipsis-vertical fs-5 "></i>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <img
-                      width={40}
-                      height={40}
-                      className="me-3 rounded-circle table-profile-img"
-                      src="https://www.growcropsonline.com/assets/img/agent-2.jpg"
-                      alt=""
-                    />
-                    <div className="user-data ">
-                      <div className="d-flex align-items-center">
-                        <h6 className="m-0">Maribel_Koss </h6>
-                        <img
-                          width={28}
-                          src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Twitter-Verified-900x0.png"
-                          alt=""
-                        />
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">Oct 01, 2022 </h6>
+                    <span className="text-secondary ">Subscribe Date</span>
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">
+                      None
+                      <i className="fa-solid fa-circle-exclamation mx-1 text-warning"></i>
+                      <span className="text-warning fw-normal">
+                        Expires soon
+                      </span>
+                    </h6>
+                    <span className="text-secondary">Renew Date</span>
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">$3.00</h6>
+                    <span className="text-secondary">Monthly Cost</span>
+                  </td>
+                  <td align="center" className="text-end">
+                    <div className="d-flex align-items-center justify-content-end mt-2">
+                      <button className="action-button btn py-2  fw-semibold border">
+                        <i className="fa-solid fa-rotate px-1"></i> Renew
+                      </button>
+                      <button className="btn ms-1">
+                        <i className="fa-solid fa-ellipsis-vertical fs-5 "></i>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        width={40}
+                        height={40}
+                        className="me-3 rounded-circle table-profile-img"
+                        src="https://www.growcropsonline.com/assets/img/agent-2.jpg"
+                        alt=""
+                      />
+                      <div className="user-data ">
+                        <div className="d-flex align-items-center">
+                          <h6 className="m-0">Maribel_Koss </h6>
+                          <img
+                            width={28}
+                            src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Twitter-Verified-900x0.png"
+                            alt=""
+                          />
+                        </div>
+                        <span className="text-secondary">@maribelk</span>
                       </div>
-                      <span className="text-secondary">@maribelk</span>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">Oct 01, 2022 </h6>
-                  <span className="text-secondary ">Subscribe Date</span>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">Apr 17, 2023</h6>
-                  <span className="text-secondary">Renew Date</span>
-                </td>
-                <td>
-                  <h6 className="m-0 mt-2">$3.00</h6>
-                  <span className="text-secondary">Monthly Cost</span>
-                </td>
-                <td align="center" className="text-end">
-                  <div className="d-flex align-items-center justify-content-end mt-2">
-                    <button className="action-button btn py-2  fw-semibold border">
-                      <i className="fa-solid fa-ban px-1"></i> Cancel
-                    </button>
-                    <button className="btn ms-1">
-                      <i className="fa-solid fa-ellipsis-vertical fs-5"></i>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">Oct 01, 2022 </h6>
+                    <span className="text-secondary ">Subscribe Date</span>
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">Apr 17, 2023</h6>
+                    <span className="text-secondary">Renew Date</span>
+                  </td>
+                  <td>
+                    <h6 className="m-0 mt-2">$3.00</h6>
+                    <span className="text-secondary">Monthly Cost</span>
+                  </td>
+                  <td align="center" className="text-end">
+                    <div className="d-flex align-items-center justify-content-end mt-2">
+                      <button className="action-button btn py-2  fw-semibold border">
+                        <i className="fa-solid fa-ban px-1"></i> Cancel
+                      </button>
+                      <button className="btn ms-1">
+                        <i className="fa-solid fa-ellipsis-vertical fs-5"></i>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </HrmsAdminLayout>
