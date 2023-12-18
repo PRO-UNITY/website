471c2312c726e135172e7b590b2a79d10a0f79bf
amirbek <amirbekazimov7@gmail.com>
2023-12-18 17:21:02 +0500

    job details

--- src/pages/Products/Hrms/demo/jobs/JobDetails.jsx
+++ src/pages/Products/Hrms/demo/jobs/JobDetails.jsx
@@ -30,9 +30,9 @@ const JobDetails = () => {
                 </div>
 
                 <div className="d-flex mt-5 w-75 flex-column gap-2">
-                    <p className="h3">About {job?.company.name}:</p>
+                    <p className="h3">About {job?.company?.name}:</p>
 
-                    <p>{job?.company.content}</p>
+                    <p>{job?.company?.content}</p>
 
                     <p>
                         Join getitAI, a trailblazer in human-centric commerce, where we're transforming the way buyers are engaged online through our autonomous AI Sellers. Watch an investor video we made: https://go.getit.ai/pier39-pitch
@@ -94,7 +94,7 @@ const JobDetails = () => {
                     <div className="card-body">
                         <div className="d-flex flex-column align-items-center gap-2 text-center">
                             <img style={{ width: 65, height: 65 }} class="card-img-top rounded-5" src={`${BASE_URL}${job?.company?.logo}`} alt="Card image cap" />
-                            <h5 className="card-title">{job?.company.name}</h5>
+                            <h5 className="card-title">{job?.company?.name}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Visit Website</h6>
                             <button className="btn btn-dark rounded-5">Apply for this position</button>
                         </div>
@@ -102,19 +102,19 @@ const JobDetails = () => {
                         <div className="d-flex flex-column gap-3">
                             <div>
                                 <span>Job Type</span>
-                                <h5 className="text-dark mt-2">{job.job_type.type}</h5>
+                                <h5 className="text-dark mt-2">{job?.job_type?.type}</h5>
                             </div>
                             <div>
                                 <span>Location</span>
-                                <h5 className="text-dark mt-2">{job.company.countries.name}</h5>
+                                <h5 className="text-dark mt-2">{job?.company?.countries.name}</h5>
                             </div>
                             <div>
                                 <span>Date posted</span>
-                                <h5 className="text-dark mt-2">{job.company.created_at}</h5>
+                                <h5 className="text-dark mt-2">{job?.company?.created_at}</h5>
                             </div>
                             <div>
                                 <span>Salary</span>
-                                <h5 className="text-danger mt-2">{job.salary}$</h5>
+                                <h5 className="text-danger mt-2">{job?.salary}$</h5>
                             </div>
                         </div>
 
