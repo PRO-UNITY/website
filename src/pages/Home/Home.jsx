8e758412e0e2f427455bcae7c2363378f74fc9a7
Jobir <jobirashurov7877@gmail.com>
2024-01-04 12:59:51 +0500

    Hero title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -38,10 +38,10 @@ const Home = () => {
       {/* Header Start */}
       <section className=" container-fluid header pro-bg-primary p-0 mb-5">
         <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
-          <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
+          <div className="col-lg-7 p-4 wow fadeIn" data-wow-delay="0.1s">
             <h1
-              style={{ fontWeight: "600" }}
-              className="display-4 text-white mb-5"
+              style={{ fontWeight: "400" }}
+              className="display-5 text-white mb-5"
             >
               {t("herosection.title")}
             </h1>
@@ -62,7 +62,7 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+          <div className="col-lg-5 wow fadeIn" data-wow-delay="0.5s">
             <div className="owl-carousel header-carousel">
               <HomeSlider />
             </div>
