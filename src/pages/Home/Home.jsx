85d042c50155e113bb558be582857cc595e6a830
Jobir <jobirashurov7877@gmail.com>
2024-01-10 14:36:13 +0500

    seo meta tags

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -34,11 +34,15 @@ const Home = () => {
     });
   };
   return (
-    <Layout>
+    <Layout
+      title={
+        "ProUnity - Your Partner in Web and Mobile Development in Bukhara."
+      }
+    >
       {/* Header Start */}
       <section className=" container-fluid header pro-bg-primary p-0 mb-5">
         <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
-          <div className="col-lg-7 p-4 wow fadeIn" data-wow-delay="0.1s">
+          <div className="col-lg-7 p-4 wow fadeIn">
             <h1
               style={{ fontWeight: "400" }}
               className="display-5 text-white mb-5"
@@ -94,7 +98,7 @@ const Home = () => {
       <div className="container-xxl py-5">
         <div className="container">
           <div className="row g-5">
-            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+            <div className="col-lg-6 wow fadeIn">
               <div className="d-flex flex-column">
                 <img
                   className="img-fluid rounded w-75 align-self-end img-lider"
@@ -148,7 +152,6 @@ const Home = () => {
         <div className="container">
           <div
             className="text-center mx-auto mb-5 wow fadeInUp"
-            data-wow-delay="0.1s"
             style={{ maxWidth: 600 }}
           >
             <p className="d-inline-block border rounded-pill py-1 px-4">
@@ -157,10 +160,7 @@ const Home = () => {
             <h1>{t("followsection.title")}</h1>
           </div>
           <div className="row g-4">
-            <div
-              className="col-lg-4 col-md-6 wow fadeInUp cursor p-3"
-              data-wow-delay="0.1s"
-            >
+            <div className="col-lg-4 col-md-6 wow fadeInUp cursor p-3">
               <a
                 target="_blank"
                 rel="noreferrer"
@@ -219,7 +219,6 @@ const Home = () => {
         <div className="container">
           <div
             className="text-center mx-auto mb-5 wow fadeInUp"
-            data-wow-delay="0.1s"
             style={{ maxWidth: 600 }}
           >
             <p className="d-inline-block border rounded-pill py-1 px-4">
@@ -240,10 +239,7 @@ const Home = () => {
       <div className="container-fluid pro-bg-primary overflow-hidden mt-5 px-lg-0">
         <div className="container-xxl feature px-lg-0">
           <div className="row g-0 mx-lg-0">
-            <div
-              className="col-lg-6 feature-text py-5 wow fadeIn"
-              data-wow-delay="0.1s"
-            >
+            <div className="col-lg-6 feature-text py-5 wow fadeIn">
               <div className="p-lg-5 ps-lg-0">
                 <p className="d-inline-block border rounded-pill text-light py-1 px-4">
                   {t("featuresection.featurebutton")}
@@ -382,7 +378,6 @@ const Home = () => {
         <div className="container">
           <div
             className="text-center mx-auto mb-5 wow fadeInUp"
-            data-wow-delay="0.1s"
             style={{ maxWidth: 600 }}
           >
             <p className="d-inline-block border rounded-pill py-1 px-4">
@@ -404,7 +399,7 @@ const Home = () => {
       <div className="container-xxl py-5 contact">
         <div className="container">
           <div className="row g-5">
-            <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div className="col-lg-6 wow fadeInUp">
               <p className="d-inline-block border rounded-pill py-1 px-4">
                 {t("appointment.appointmentbutton")}
               </p>
