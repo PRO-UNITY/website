1d9f3c727bf2d125afe06e4328ec15ecf1850327
naimovdavlatshoh <you@example.com>
2024-02-22 18:03:54 +0500

    added all projects

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,184 +8,228 @@ import "./Navbar.css";
 import { useState } from "react";
 
 const MyNavbar = () => {
-  const { t } = useTranslation();
-  const [activeLang, setactiveLang] = useState("en");
-  const { pathname } = useLocation();
-  const changeLang = (lang) => {
-    i18next.changeLanguage(lang);
-    setactiveLang(document.cookie.split("=")[1] || "en");
-  };
+    const { t } = useTranslation();
+    const [activeLang, setactiveLang] = useState("en");
+    const { pathname } = useLocation();
+    const changeLang = (lang) => {
+        i18next.changeLanguage(lang);
+        setactiveLang(document.cookie.split("=")[1] || "en");
+    };
 
-  return (
-    <>
-      <Topbar />
-      <Navbar
-        style={{ backgroundColor: "yellow" }}
-        expand="lg"
-        className="sticky-top navbar"
-      >
-        <div className="container-fluid">
-          <Navbar.Brand as={Link} to="/" className="logo">
-            <img className="ml-0 " src={Logo} alt="prounity-logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarCollapse" />
-          <Navbar.Collapse id="navbarCollapse">
-            <Nav className="ms-auto p-4 p-lg-0 align-items-center">
-              <NavLink
-                className={`${pathname == "/" ? "active" : ""} py-0`}
-                as={Link}
-                to="/"
-              >
-                {t("navbar.home")}
-              </NavLink>
-              <NavLink
-                as={Link}
-                to="/about"
-                className={`${pathname.includes("/about") && "active"} py-0`}
-              >
-                {t("navbar.about")}
-              </NavLink>
-              <Nav.Link
-                as={Link}
-                to="/services"
-                className={`${pathname.includes("/services") && "active"} py-0`}
-              >
-                {t("navbar.service")}
-              </Nav.Link>
-              <div className="nav-item dropdown">
-                <NavLink
-                  as={Link}
-                  to={"/products"}
-                  className={`py-0 nav-link dropdown-toggle ${pathname.includes("/products") && "active"
-                    }`}
-                >
-                  {t("navbar.products")}
-                </NavLink>
-                <div className="dropdown-menu  rounded px-3 mt-2">
-                  <Link
-                    to={"/products/hrms"}
-                    className={`dropdown-item px-0 ${pathname === "/products/hrms"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Hrms
-                  </Link>
-                  <Link
-                    to={"/products/delivery"}
-                    className={`dropdown-item px-0 ${pathname === "/products/delivery"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Delivery
-                  </Link>
-                  <Link
-                    to={"/products/chat-app"}
-                    className={`dropdown-item px-0 ${pathname === "/products/chat-app"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Chat
-                  </Link>
-                  <Link
-                    to={"/products/clinic-app"}
-                    className={`dropdown-item px-0 ${pathname === "/products/clinic-app"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Clinic
-                  </Link>
-                  <Link
-                    to={"/products/todo-app"}
-                    className={`dropdown-item px-0 ${pathname === "/products/todo-app"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Todo
-                  </Link>
-                  <Link
-                    to={"/products/spa-app"}
-                    className={`dropdown-item px-0 ${pathname === "/products/spa-app"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    SPA
-                  </Link>
-                  <Link
-                    to={"/products/authentication"}
-                    className={`dropdown-item px-0 ${pathname === "/products/authentication"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Authentication
-                  </Link>
-                  <Link
-                    to={"/products/store"}
-                    className={`dropdown-item px-0 ${pathname === "/products/store"
-                        ? "dropdown-item-active"
-                        : ""
-                      }`}
-                  ></Link>
-                  Store
-                  <Link
-                    to={"/products/real-estate"}
-                    className={`dropdown-item px-0 ${pathname === "/products/real-estate"
-                      ? "dropdown-item-active"
-                      : ""
-                      }`}
-                  >
-                    Real-Estate
-                  </Link>
-                </div>
-              </div>
+    return (
+        <>
+            <Topbar />
+            <Navbar
+                style={{ backgroundColor: "yellow" }}
+                expand="lg"
+                className="sticky-top navbar"
+            >
+                <div className="container-fluid">
+                    <Navbar.Brand as={Link} to="/" className="logo">
+                        <img className="ml-0 " src={Logo} alt="prounity-logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarCollapse" />
+                    <Navbar.Collapse id="navbarCollapse">
+                        <Nav className="ms-auto p-4 p-lg-0 align-items-center">
+                            <NavLink
+                                className={`${
+                                    pathname == "/" ? "active" : ""
+                                } py-0`}
+                                as={Link}
+                                to="/"
+                            >
+                                {t("navbar.home")}
+                            </NavLink>
+                            <NavLink
+                                as={Link}
+                                to="/about"
+                                className={`${
+                                    pathname.includes("/about") && "active"
+                                } py-0`}
+                            >
+                                {t("navbar.about")}
+                            </NavLink>
+                            <Nav.Link
+                                as={Link}
+                                to="/services"
+                                className={`${
+                                    pathname.includes("/services") && "active"
+                                } py-0`}
+                            >
+                                {t("navbar.service")}
+                            </Nav.Link>
+                            <div className="nav-item dropdown">
+                                <NavLink
+                                    as={Link}
+                                    to={"/products"}
+                                    className={`py-0 nav-link dropdown-toggle ${
+                                        pathname.includes("/products") &&
+                                        "active"
+                                    }`}
+                                >
+                                    {t("navbar.products")}
+                                </NavLink>
+                                <div className="dropdown-menu  rounded px-3 mt-2">
+                                    <Link
+                                        to={"/products/hrms"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/hrms"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Hrms
+                                    </Link>
+                                    <Link
+                                        to={"/products/delivery"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/delivery"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Delivery
+                                    </Link>
+                                    <Link
+                                        to={"/products/chat-app"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/chat-app"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Chat
+                                    </Link>
+                                    <Link
+                                        to={"/products/clinic-app"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/clinic-app"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Clinic
+                                    </Link>
+                                    <Link
+                                        to={"/products/todo-app"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/todo-app"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Todo
+                                    </Link>
+                                    <Link
+                                        to={"/products/spa-app"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/spa-app"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        SPA
+                                    </Link>
+                                    <Link
+                                        to={"/products/authentication"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname ===
+                                            "/products/authentication"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Authentication
+                                    </Link>
+                                    <Link
+                                        to={"/products/store"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/store"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Store
+                                    </Link>
 
-              <Nav.Link
-                as={Link}
-                to="/contact"
-                className={`${pathname.includes("/contact") && "active"} py-0 `}
-              >
-                {t("navbar.contact")}
-              </Nav.Link>
-              <div className="nav-item dropdown ">
-                <a href="#" className={`py-0 nav-link dropdown-toggle`}>
-                  {t("navbar.language")}
-                </a>
-                <div className="dropdown-menu lang-menu  rounded pb-3  px-3 mt-2">
-                  <span
-                    className={`dropdown-item px-0 ${activeLang === "en" ? "dropdown-item-active " : ""
-                      } `}
-                    onClick={() => changeLang("en")}
-                  >
-                    En
-                  </span>
-                  <span
-                    className={`dropdown-item px-0 ${activeLang === "ru" ? "dropdown-item-active " : ""
-                      } `}
-                    onClick={() => changeLang("ru")}
-                  >
-                    Ru
-                  </span>
-                  <span
-                    className={`dropdown-item px-0 ${activeLang === "uz" ? "dropdown-item-active " : ""
-                      } `}
-                    onClick={() => changeLang("uz")}
-                  >
-                    Uz
-                  </span>
+                                    <Link
+                                        to={"/products/real-estate"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/real-estate"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Real-Estate
+                                    </Link>
+                                    <Link
+                                        to={"/products/qr-app"}
+                                        className={`dropdown-item px-0 ${
+                                            pathname === "/products/qr-app"
+                                                ? "dropdown-item-active"
+                                                : ""
+                                        }`}
+                                    >
+                                        Qr-app
+                                    </Link>
+                                </div>
+                            </div>
+
+                            <Nav.Link
+                                as={Link}
+                                to="/contact"
+                                className={`${
+                                    pathname.includes("/contact") && "active"
+                                } py-0 `}
+                            >
+                                {t("navbar.contact")}
+                            </Nav.Link>
+                            <div className="nav-item dropdown ">
+                                <a
+                                    href="#"
+                                    className={`py-0 nav-link dropdown-toggle`}
+                                >
+                                    {t("navbar.language")}
+                                </a>
+                                <div className="dropdown-menu lang-menu  rounded pb-3  px-3 mt-2">
+                                    <span
+                                        className={`dropdown-item px-0 ${
+                                            activeLang === "en"
+                                                ? "dropdown-item-active "
+                                                : ""
+                                        } `}
+                                        onClick={() => changeLang("en")}
+                                    >
+                                        En
+                                    </span>
+                                    <span
+                                        className={`dropdown-item px-0 ${
+                                            activeLang === "ru"
+                                                ? "dropdown-item-active "
+                                                : ""
+                                        } `}
+                                        onClick={() => changeLang("ru")}
+                                    >
+                                        Ru
+                                    </span>
+                                    <span
+                                        className={`dropdown-item px-0 ${
+                                            activeLang === "uz"
+                                                ? "dropdown-item-active "
+                                                : ""
+                                        } `}
+                                        onClick={() => changeLang("uz")}
+                                    >
+                                        Uz
+                                    </span>
+                                </div>
+                            </div>
+                        </Nav>
+                    </Navbar.Collapse>
                 </div>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
-    </>
-  );
+            </Navbar>
+        </>
+    );
 };
 
 export default MyNavbar;
